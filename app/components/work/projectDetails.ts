945d8aa18cf2a81fc8cf7ab1763399436ccdf036
YG <143325418+VecnaJS@users.noreply.github.com>
2023-10-05 02:58:12 +0300

    Add files via upload

---
+++ app/components/work/projectDetails.ts
@@ -0,0 +1,41 @@
+import {
+    SiCplusplus,
+    SiFramer,
+    SiGithub, SiNeovim,
+    SiNextdotjs,
+    SiReact,
+    SiRust,
+    SiTailwindcss,
+    SiTypescript,
+    SiZig
+} from "react-icons/si";
+import {IconType} from "react-icons";
+
+export type ProjectProps = {
+  id: number;
+  name: string;
+  description: string;
+  technologies: IconType[];
+  techNames: string[];
+  techLinks: string[];
+  github: string;
+  demo: string;
+  image: string;
+  available: boolean;
+};
+
+export const projects = [
+    {
+        id: 0,
+        name: "... 2024",
+        description:
+            "waiting for the year 2024 for the big day",
+        technologies: [SiTypescript, SiReact, SiNextdotjs, SiTailwindcss, SiFramer],
+        techNames: ["TypeScript", "React", "Next.js", "Tailwind CSS", "Framer Motion"],
+        techLinks: ["https://www.typescriptlang.org/", "https://reactjs.org/", "https://nextjs.org/", "https://tailwindcss.com/", "https://www.framer.com/motion/"],
+        github: "#",
+        demo: "#",
+        image: "/projects/200a0.webp",
+        available: true,
+    }
+];
